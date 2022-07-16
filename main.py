416c03673b0d1c8af972a946f9c96bcb07581f79
ErrorNoInternet <error.nointernet@gmail.com>
2022-07-16 16:31:03 +0800

    Increase logs

--- main.py
+++ main.py
@@ -42,12 +42,12 @@ def update_video():
                         index += 1
                         continue
                 if id not in video_data.keys():
-                    print(f"Adding {id}")
+                    print(f"Adding {id}...")
                     stream_data = requests.get(url).content
                     video_data[id] = stream_data
                 if len(video_data) > buffer_limit:
                     id = min(video_data.keys())
-                    print(f"Removing {id}")
+                    print(f"Removing {id}...")
                     del video_data[id]
         except Exception as exception:
             print(f"Failed to fetch video: {exception}")
